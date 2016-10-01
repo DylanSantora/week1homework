6f9fd678c6bac2cd28abb3239693cf4a569f2885
DylanSantora <DylanSantora>
2016-09-30 23:45:07 -0700

    Daniel's

---
+++ week4homeworkDaniel/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char* argv[]){
+	string imageLocation = string(argv[1]);
+	Mat img = imread(imageLocation);
+
+	imshow(imageLocation, img);
+	waitKey(0);
+	return(0);
+}
